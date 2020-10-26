aa69e2e69f2c67cbd0a25e16beafae180fbd1e81
odeane_croney <odeane_croney@yahoo.com>
2020-10-26 21:28:07 +0100

    rendering products content

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,20 +4,42 @@ import { fetchProducts } from '../../state/actions/productActions'
 import { useEffect } from 'react';
 
 function Products(props) {
-  
+
+  let products = props.products
+
   useEffect(() => {
     props.fetchProducts()
   }, [])
-  
+
+
+  const productList = products ? (
+    products.map((product) => {
+      return (
+        <div key={product._id} className="product">
+          <h1>{product.name}</h1>
+          <p>{product.m1}</p>
+          <p>{product.m2}</p>
+          <span>2 marcador</span>
+          <span>{product.price}{product.unit}</span>
+          <button>Add To Cart</button>
+        </div>
+      )
+    })
+  ) : (
+      <div>No products found!!</div>
+    )
+
   return (
-    <div>
-      <h1>Products</h1>
+    <div className='products'>
+      {productList}
     </div>
   )
 }
 
 const mapStateToProps = (state) => {
-  console.log(state)
+  return {
+    products: state.products.docs,
   }
+}
 
 export default connect(mapStateToProps, { fetchProducts })(Products)
