be0a88d4cf9f3940efd9b85afe6726dc26502018
odeane_croney <odeane_croney@yahoo.com>
2020-10-28 20:09:41 +0100

    adding modal from material ui

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,16 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import Badge from '@material-ui/core/Badge'
+import Popover from '@material-ui/core/Popover';
+
+
 
 function Header() {
+  const [anchorEl, setAnchorEl] = useState(null)
+  const handleClick = e => {
+    debugger
+    setAnchorEl(e.currentTarget)
+  }
+  const handleClose = () => setAnchorEl(null)
 
-  
+  const open = Boolean(anchorEl);
 
   return (
     <nav className='header-container'>
       <Badge badgeContent={4} color='primary'>
-      <ShoppingCartIcon className='cart-icon' />
+        <ShoppingCartIcon onClick={handleClick} className='cart-icon' />
       </Badge>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+      <h1>Testing modal</h1>
+      </Popover>
       </nav>
   )
 }
