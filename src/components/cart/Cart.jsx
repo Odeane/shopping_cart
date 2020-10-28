5db1da6927b29ca56aaf63df9debd864e4776834
odeane_croney <odeane_croney@yahoo.com>
2020-10-28 20:21:55 +0100

    refactoring modal to its own component

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import Popover from '@material-ui/core/Popover';
+
+function Cart({anchorEl, handleClose}) {
+
+
+  const open = Boolean(anchorEl);
+  return (
+    <div>
+      <Popover
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left'
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center'
+        }}
+      >
+        <h1>Testing</h1>
+      </Popover>
+    </div>
+  )
+}
+
+export default Cart
