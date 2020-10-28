0c61ada8fe30dc525073b104443415f66621a3a0
odeane_croney <odeane_croney@yahoo.com>
2020-10-28 19:15:31 +0100

    adding shopping cart and badge to header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
+import Badge from '@material-ui/core/Badge'
 
 function Header() {
+
+  
+
   return (
-    <nav>
-      
-    </nav>
+    <nav className='header-container'>
+      <Badge badgeContent={4} color='primary'>
+      <ShoppingCartIcon className='cart-icon' />
+      </Badge>
+      </nav>
   )
 }
 
