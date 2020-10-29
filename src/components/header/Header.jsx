46e1e6a35d9d808ffa0260c3764c96b7d57f1965
odeane_croney <odeane_croney@yahoo.com>
2020-10-29 12:59:37 +0100

    adding hover effect to cart icon and updating quantity change on the cart

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,15 +3,17 @@ import ShoppingCartIcon from '@material-ui/icons/ShoppingCart'
 import Badge from '@material-ui/core/Badge'
 import Cart from '../cart/Cart';
 import './header.css'
+import { connect } from 'react-redux';
 
-function Header() {
+function Header({cartProducts}) {
+
   const [anchorEl, setAnchorEl] = useState(null)
   const handleClick = e => setAnchorEl(e.currentTarget)
   const handleClose = () => setAnchorEl(null)
 
   return (
     <nav className='header__container'>
-      <Badge badgeContent={4} color='primary'>
+      <Badge badgeContent={cartProducts.length} color='primary'>
         <ShoppingCartIcon onClick={handleClick} className='cart__icon' />
       </Badge>
       <Cart anchorEl={anchorEl} handleClose={handleClose} />
@@ -19,4 +21,10 @@ function Header() {
   )
 }
 
-export default Header
+const mapStateToProps = ({cartStatus}) => {
+  return {
+     cartProducts: cartStatus.cartProducts
+  }
+}
+
+export default connect(mapStateToProps, {})(Header)
